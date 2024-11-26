c04608accc6f51e4f477433a7eea96cca08100c0
Harini4121 <srivyshnavi4121@gmailcom>
2024-11-26 16:45:54 +0530

    importing and exporting modules

---
+++ index.js
@@ -0,0 +1,2 @@
+const add = require('./calculator');
+console.log(add(26,4));
---
+++ indexM.js
@@ -0,0 +1,4 @@
+const cal = require('./calculatorM');
+const {add, sub} = cal;
+console.log(add(26,4));
+console.log(sub(26,4));
